3e026b981a5439813b844d537b80da1fec2e1251
Matheus Reis <matheusgnreis@gmail.com>
2022-09-04 04:17:18 -0300

    chore: test of solution

--- functions/routes/ecom/modules/list-payments.js
+++ functions/routes/ecom/modules/list-payments.js
@@ -21,54 +21,6 @@ exports.post = ({ appSdk }, req, res) => {
     payment_gateways: []
   }
 
-  const { discount } = config
-  if (discount && discount.value > 0) {
-    if (discount.apply_at !== 'freight') {
-      // default discount option
-      const { value } = discount
-      response.discount_option = {
-        label: config.discount_option_label,
-        value
-      }
-      // specify the discount type and min amount is optional
-      ;['type', 'min_amount'].forEach(prop => {
-        if (discount[prop]) {
-          response.discount_option[prop] = discount[prop]
-        }
-      })
-    }
-
-    if (amount.total) {
-      // check amount value to apply discount
-      if (amount.total < discount.min_amount) {
-        discount.value = 0
-      } else {
-        delete discount.min_amount
-
-        // fix local amount object
-        const maxDiscount = amount[discount.apply_at || 'subtotal']
-        let discountValue
-        if (discount.type === 'percentage') {
-          discountValue = maxDiscount * discount.value / 100
-        } else {
-          discountValue = discount.value
-          if (discountValue > maxDiscount) {
-            discountValue = maxDiscount
-          }
-        }
-        if (discountValue > 0) {
-          amount.discount = (amount.discount || 0) + discountValue
-          amount.total -= discountValue
-          console.log('Discount to be applied', amount)
-          console.log('Discount object', discount)
-          if (amount.total < 0) {
-            amount.total = 0
-          }
-        }
-      }
-    }
-  }
-
   // setup payment gateway objects
   const intermediator = {
     name: 'Pagar.me',
@@ -98,6 +50,60 @@ exports.post = ({ appSdk }, req, res) => {
         },
         intermediator
       }
+      const { discount } = config
+      if (discount && discount.value > 0) {
+        if (discount.apply_at !== 'freight') {
+          // default discount option
+          const { value } = discount
+          response.discount_option = {
+            label: config.discount_option_label,
+            value
+          }
+          // specify the discount type and min amount is optional
+          ;['type', 'min_amount'].forEach(prop => {
+            if (discount[prop]) {
+              response.discount_option[prop] = discount[prop]
+            }
+          })
+        }
+        if (methodConfig.discount) {
+          gateway.discount = methodConfig.discount
+          if (amount.total) {
+            // check amount value to apply discount
+            if (amount.total < discount.min_amount) {
+              discount.value = 0
+            } else {
+              delete discount.min_amount
+              // fix local amount object
+              const maxDiscount = amount[discount.apply_at || 'subtotal']
+              let discountValue
+              if (discount.type === 'percentage') {
+                discountValue = maxDiscount * discount.value / 100
+              } else {
+                discountValue = discount.value
+                if (discountValue > maxDiscount) {
+                  discountValue = maxDiscount
+                }
+              }
+              if (discountValue > 0) {
+                amount.discount = (amount.discount || 0) + discountValue
+                amount.total -= discountValue
+                if (amount.total < 0) {
+                  amount.total = 0
+                }
+              }
+            }
+          }
+        } else if (
+          discount &&
+          (discount[paymentMethod] === true || (!isCreditCard && discount[paymentMethod] !== false))
+        ) {
+          gateway.discount = discount
+          if (response.discount_option && !response.discount_option.label) {
+            response.discount_option.label = label
+          }
+        }
+      }
 
       if (isCreditCard) {
         if (!gateway.icon) {
@@ -116,28 +122,9 @@ exports.post = ({ appSdk }, req, res) => {
         const { installments } = config
         if (installments) {
           // list all installment options and default one
-          const total = params.amount || amount
-          console.log('First amount money', total)
-          if (total.discount) {
-            total.total = total.subtotal
-          }
-          console.log('Second amount money', total)
-          addInstallments(total, installments, gateway, response)
+          addInstallments(amount, installments, gateway, response)
         }
       }
-      console.log('Metodo para desconto', JSON.stringify(methodConfig))
-      if (methodConfig.discount) {
-        gateway.discount = methodConfig.discount
-      } else if (
-        discount &&
-        (discount[paymentMethod] === true || (!isCreditCard && discount[paymentMethod] !== false))
-      ) {
-        gateway.discount = discount
-        if (response.discount_option && !response.discount_option.label) {
-          response.discount_option.label = label
-        }
-      }
-      console.log('Gateway', JSON.stringify(gateway))
       response.payment_gateways.push(gateway)
     }
   })
