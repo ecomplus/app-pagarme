9133ac9a9f7372251a3dd0d64b3383843149afee
matheusgnreis <matheus@e-com.club>
2023-03-29 21:06:39 -0300

    chore(onload): simplify code

--- functions/public/onload-expression.js
+++ functions/public/onload-expression.js
@@ -2,14 +2,15 @@
   window._pagarmeHash = function (card) {
     return new Promise(function (resolve, reject) {
       // https://github.com/pagarme/pagarme-js#using-encryption-key
-      const validateObject = window.pagarme.validate({ 
+      const usedCard = { 
         card: {
           card_number: card.number,
           card_holder_name: card.name,
           card_expiration_date: card.month.toString() + card.year.toString(),
           card_cvv: card.cvc
         }
-      })
+      }
+      const validateObject = window.pagarme.validate(usedCard)
       const objectCardValidated = validateObject && validateObject.card
       for (let key in objectCardValidated) {
         if (Object.hasOwnProperty.call(objectCardValidated, key)) {
@@ -20,13 +21,7 @@
       }
       window.pagarme.client.connect({ encryption_key: window._pagarmeKey })
         .then(function (client) {
-
-          return client.security.encrypt({
-            card_number: card.number,
-            card_holder_name: card.name,
-            card_expiration_date: card.month.toString() + card.year.toString(),
-            card_cvv: card.cvc
-          })
+          return client.security.encrypt(usedCard.card)
         })
         .then(resolve)
         .catch(reject) 
