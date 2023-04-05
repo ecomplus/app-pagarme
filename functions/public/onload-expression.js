c8725669b53c1c6f2a86a8df4e1abcf023c5bcaa
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-04-05 12:18:34 -0300

    fix(onload-expression): validate card with pagarme.js before card hash (#94) [skip ci]
    * chore(onload): validate card before card hash
    
    * chore(onload): simplify code
    
    * fix(onload-expression): reject with new Error

--- functions/public/onload-expression.js
+++ functions/public/onload-expression.js
@@ -2,17 +2,29 @@
   window._pagarmeHash = function (card) {
     return new Promise(function (resolve, reject) {
       // https://github.com/pagarme/pagarme-js#using-encryption-key
+      const usedCard = { 
+        card: {
+          card_number: card.number,
+          card_holder_name: card.name,
+          card_expiration_date: card.month.toString() + card.year.toString(),
+          card_cvv: card.cvc
+        }
+      }
+      const validateObject = window.pagarme.validate(usedCard)
+      const objectCardValidated = validateObject && validateObject.card
+      for (let key in objectCardValidated) {
+        if (Object.hasOwnProperty.call(objectCardValidated, key)) {
+          if (!objectCardValidated[key]) {
+            return reject(new Error(`Invalid card: ${key}`))
+          }
+        }
+      }
       window.pagarme.client.connect({ encryption_key: window._pagarmeKey })
         .then(function (client) {
-          return client.security.encrypt({
-            card_number: card.number,
-            card_holder_name: card.name,
-            card_expiration_date: card.month.toString() + card.year.toString(),
-            card_cvv: card.cvc
-          })
+          return client.security.encrypt(usedCard.card)
         })
         .then(resolve)
-        .catch(reject)
+        .catch(reject) 
     })
   }
 }())
